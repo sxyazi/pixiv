6eaa43129e2d66c2724ff570e5529787a7bbc1ae
sxyazi <sxyazi@gmail.com>
2021-01-31 16:08:57 +0800

    Initial Commit

---
+++ Pixiv/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pixiv
+//
+//  Created by fuwaika on 2021/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pixiv/PixivApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixivApp.swift
+//  Pixiv
+//
+//  Created by fuwaika on 2021/1/31.
+//
+
+import SwiftUI
+
+@main
+struct PixivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
